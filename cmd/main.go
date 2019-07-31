2ff537fdc3e4c722094b63dbcc05652ddb392cc0
Yusuke Hata <yusuke.hata@mirrativ.co.jp>
2019-07-31 13:16:10 +0900

    change from go/flags to cli

--- cmd/main.go
+++ cmd/main.go
@@ -1,139 +1,220 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"net"
-	"os"
-	"regexp"
-	"strings"
+  "log"
+  "fmt"
+  "net"
+  "os"
+  "regexp"
+  "strings"
 
-	"github.com/lumanetworks/go-tcp-proxy"
+  "github.com/comail/colog"
+  "gopkg.in/urfave/cli.v1"
+
+  "github.com/octu0/go-tcp-proxy"
 )
 
 var (
-	matchid = uint64(0)
-	connid  = uint64(0)
-	logger  proxy.ColorLogger
-
-	localAddr   = flag.String("l", ":9999", "local address")
-	remoteAddr  = flag.String("r", "localhost:80", "remote address")
-	verbose     = flag.Bool("v", false, "display server actions")
-	veryverbose = flag.Bool("vv", false, "display server actions and all tcp data")
-	nagles      = flag.Bool("n", false, "disable nagles algorithm")
-	hex         = flag.Bool("h", false, "output hex")
-	colors      = flag.Bool("c", false, "output ansi colors")
-	unwrapTLS   = flag.Bool("unwrap-tls", false, "remote connection with TLS exposed unencrypted locally")
-	match       = flag.String("match", "", "match regex (in the form 'regex')")
-	replace     = flag.String("replace", "", "replace regex (in the form 'regex~replacer')")
+  matchid = uint64(0)
+  connid  = uint64(0)
+  logger  proxy.ColorLogger
 )
 
-func main() {
-	flag.Parse()
+var (
+  Commands = make([]cli.Command, 0)
+)
+func AddCommand(cmd cli.Command){
+  Commands = append(Commands, cmd)
+}
 
-	logger := proxy.ColorLogger{
-		Verbose: *verbose,
-		Color:   *colors,
-	}
+func action(c *cli.Context) error {
+  localAddr  := c.String("local")
+  remoteAddr := c.String("remote")
+  nagles     := c.Bool("no-nagles")
+  unwrapTLS  := c.Bool("unwrap-tls")
+  match      := c.String("match")
+  replace    := c.String("replace")
 
-	logger.Info("Proxying from %v to %v", *localAddr, *remoteAddr)
+  outputHex  := c.Bool("hex")
+  debug      := c.Bool("debug")
+  verbose    := c.Bool("verbose")
 
-	laddr, err := net.ResolveTCPAddr("tcp", *localAddr)
-	if err != nil {
-		logger.Warn("Failed to resolve local address: %s", err)
-		os.Exit(1)
-	}
-	raddr, err := net.ResolveTCPAddr("tcp", *remoteAddr)
-	if err != nil {
-		logger.Warn("Failed to resolve remote address: %s", err)
-		os.Exit(1)
-	}
-	listener, err := net.ListenTCP("tcp", laddr)
-	if err != nil {
-		logger.Warn("Failed to open local port to listen: %s", err)
-		os.Exit(1)
-	}
+  log.Printf("info: starting up %s", proxy.UA)
 
-	matcher := createMatcher(*match)
-	replacer := createReplacer(*replace)
+  laddr, err := net.ResolveTCPAddr("tcp", localAddr)
+  if err != nil {
+    log.Printf("error: Failed to resolve local address(%s): %s", localAddr, err.Error())
+    return err
+  }
+  raddr, err := net.ResolveTCPAddr("tcp", remoteAddr)
+  if err != nil {
+    log.Printf("error: Failed to resolve remote address(%s): %s", remoteAddr, err.Error())
+    return err
+  }
+  listener, err := net.ListenTCP("tcp", laddr)
+  if err != nil {
+    log.Printf("error: Failed to open local port to listen(%s): %s", laddr, err.Error())
+    return err
+  }
 
-	if *veryverbose {
-		*verbose = true
-	}
+  log.Printf("info: Proxying from %s to %s", localAddr, remoteAddr)
 
-	for {
-		conn, err := listener.AcceptTCP()
-		if err != nil {
-			logger.Warn("Failed to accept connection '%s'", err)
-			continue
-		}
-		connid++
+  matcher := createMatcher(match)
+  replacer := createReplacer(replace)
 
-		var p *proxy.Proxy
-		if *unwrapTLS {
-			logger.Info("Unwrapping TLS")
-			p = proxy.NewTLSUnwrapped(conn, laddr, raddr, *remoteAddr)
-		} else {
-			p = proxy.New(conn, laddr, raddr)
-		}
+  if verbose {
+    debug = true
+  }
+  for {
+    conn, err := listener.AcceptTCP()
+    if err != nil {
+      log.Printf("warn: Failed to accept connection '%s'", err.Error())
+      continue
+    }
+    connid++
 
-		p.Matcher = matcher
-		p.Replacer = replacer
+    var p *proxy.Proxy
+    if unwrapTLS {
+      logger.Info("Unwrapping TLS")
+      p = proxy.NewTLSUnwrapped(conn, laddr, raddr, remoteAddr)
+    } else {
+      p = proxy.New(conn, laddr, raddr)
+    }
 
-		p.Nagles = *nagles
-		p.OutputHex = *hex
-		p.Log = proxy.ColorLogger{
-			Verbose:     *verbose,
-			VeryVerbose: *veryverbose,
-			Prefix:      fmt.Sprintf("Connection #%03d ", connid),
-			Color:       *colors,
-		}
+    p.Matcher = matcher
+    p.Replacer = replacer
 
-		go p.Start()
-	}
+    p.Nagles = nagles
+    p.OutputHex = outputHex
+    p.Log = proxy.ColorLogger{
+      Verbose:     debug,
+      VeryVerbose: verbose,
+      Prefix:      fmt.Sprintf("Connection #%03d ", connid),
+      Color:       false,
+    }
+
+    go p.Start()
+
+  }
+
+  return nil
 }
 
 func createMatcher(match string) func([]byte) {
-	if match == "" {
-		return nil
-	}
-	re, err := regexp.Compile(match)
-	if err != nil {
-		logger.Warn("Invalid match regex: %s", err)
-		return nil
-	}
+  if match == "" {
+    return nil
+  }
+  re, err := regexp.Compile(match)
+  if err != nil {
+    logger.Warn("Invalid match regex: %s", err)
+    return nil
+  }
 
-	logger.Info("Matching %s", re.String())
-	return func(input []byte) {
-		ms := re.FindAll(input, -1)
-		for _, m := range ms {
-			matchid++
-			logger.Info("Match #%d: %s", matchid, string(m))
-		}
-	}
+  logger.Info("Matching %s", re.String())
+  return func(input []byte) {
+    ms := re.FindAll(input, -1)
+    for _, m := range ms {
+      matchid++
+      logger.Info("Match #%d: %s", matchid, string(m))
+    }
+  }
 }
 
 func createReplacer(replace string) func([]byte) []byte {
-	if replace == "" {
-		return nil
-	}
-	//split by / (TODO: allow slash escapes)
-	parts := strings.Split(replace, "~")
-	if len(parts) != 2 {
-		logger.Warn("Invalid replace option")
-		return nil
-	}
+  if replace == "" {
+    return nil
+  }
+  //split by / (TODO: allow slash escapes)
+  parts := strings.Split(replace, "~")
+  if len(parts) != 2 {
+    logger.Warn("Invalid replace option")
+    return nil
+  }
 
-	re, err := regexp.Compile(string(parts[0]))
-	if err != nil {
-		logger.Warn("Invalid replace regex: %s", err)
-		return nil
-	}
+  re, err := regexp.Compile(string(parts[0]))
+  if err != nil {
+    logger.Warn("Invalid replace regex: %s", err)
+    return nil
+  }
 
-	repl := []byte(parts[1])
+  repl := []byte(parts[1])
 
-	logger.Info("Replacing %s with %s", re.String(), repl)
-	return func(input []byte) []byte {
-		return re.ReplaceAll(input, repl)
-	}
+  logger.Info("Replacing %s with %s", re.String(), repl)
+  return func(input []byte) []byte {
+    return re.ReplaceAll(input, repl)
+  }
+}
+
+func main(){
+  colog.SetDefaultLevel(colog.LDebug)
+  colog.SetMinLevel(colog.LInfo)
+
+  colog.SetFormatter(&colog.StdFormatter{
+    Flag: log.Ldate | log.Ltime | log.Lshortfile,
+  })
+  colog.Register()
+
+  app         := cli.NewApp()
+  app.Version  = proxy.Version
+  app.Name     = proxy.AppName
+  app.Author   = ""
+  app.Email    = ""
+  app.Usage    = ""
+  app.Action   = action
+  app.Commands = Commands
+  app.Flags    = []cli.Flag{
+    cli.StringFlag{
+      Name: "l, local",
+      Usage: "local address",
+      Value: ":9999",
+      EnvVar: "TCPPROXY_LOCALADDR",
+    },
+    cli.StringFlag{
+      Name: "r, remote",
+      Usage: "remote(destination) address",
+      Value: "127.0.0.1:80",
+      EnvVar: "TCPPROXY_REMOTEADDR",
+    },
+    cli.BoolFlag{
+      Name: "n, no-nagles",
+      Usage: "disable nagles algorithm",
+      EnvVar: "TCPPROXY_DISABLE_NAGLES",
+    },
+    cli.BoolFlag{
+      Name: "unwrap-tls",
+      Usage: "remote connection with TLS exposed unencrypted locally",
+      EnvVar: "TCPPROXY_UNWRAP_TLS",
+    },
+    cli.StringFlag{
+      Name: "match",
+      Usage: "match regex(in the form `regex`)",
+      Value: "",
+      EnvVar: "TCPPROXY_MATCH",
+    },
+    cli.StringFlag{
+      Name: "replace",
+      Usage: "replace regex(in the form `regex-replacer`)",
+      Value: "",
+      EnvVar: "TCPPROXY_REPLACE",
+    },
+    cli.BoolFlag{
+      Name: "d, debug",
+      Usage: "display server actions",
+      EnvVar: "TCPPROXY_DEBUG",
+    },
+    cli.BoolFlag{
+      Name: "V, verbose",
+      Usage: "display server actions and all tcp data",
+      EnvVar: "TCPPROXY_VERBOSE",
+    },
+    cli.BoolFlag{
+      Name: "hex",
+      Usage: "output binary data hexdecimal",
+      EnvVar: "TCPPROXY_OUTPUT_HEX",
+    },
+  }
+  if err := app.Run(os.Args); err != nil {
+    log.Printf("error: %s", err.Error())
+    cli.OsExiter(1)
+  }
 }
---
+++ version.go
@@ -0,0 +1,7 @@
+package proxy
+
+const (
+  AppName string = "tcp-proxy"
+  Version string = "1.0.0"
+  UA      string = AppName + "/" + Version
+)
